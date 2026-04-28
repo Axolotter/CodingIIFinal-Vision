12c2d72d21ea74be601bc025bb42197a31458355
Axolotter <115641617+Axolotter@users.noreply.github.com>
2026-04-28 19:15:55 +0000

    tests fixed more + code review template added

--- test_network_test.py
+++ test_network_test.py
@@ -1,15 +1,17 @@
 from network_test import predictImg
 from PIL import Image
 
-def test_pred():
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/1_quarter_1.png")) == 0
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/1_quarter_2.png")) == 0
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/3_quarter_1.png")) == 0
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/3_quarter_2.png")) == 0
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/4_quarter_2.png")) == 0
+def test_pred_no():
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/1_quarter_1.png")) == 1
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/1_quarter_3.png")) == 1
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/2_quarter_1.png")) == 1
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/3_quarter_2.png")) == 1
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/no_fuel/4_quarter_2.png")) == 1
 
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/0_quarter_1.png")) == 1
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/0_quarter_2.png")) == 1
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/0_quarter_3.png")) == 1
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/0_quarter_4.png")) == 1
-    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/1_quarter_4.png")) == 1
+
+def test_pred_fuel():
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/0_quarter_3.png")) == 0
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/0_quarter_4.png")) == 0
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/1_quarter_2.png")) == 0
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/2_quarter_3.png")) == 0
+    assert predictImg(Image.open("Rebuilt-1/test-quarters/fuel/3_quarter_3.png")) == 0
